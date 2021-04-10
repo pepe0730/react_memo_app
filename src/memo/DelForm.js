9d77be134e7cdb1dc3b505e0cf35959a53001ec4
shunpei.kanbayashi <gkhandball1@gmail.com>
2021-04-10 15:05:04 +0900

    add DelForm Component

--- src/memo/DelForm.js
+++ src/memo/DelForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { addMemo } from './Store';
+import { deleteMemo } from './Store';
 
-class AddForm extends Component {
+class DelForm extends Component {
   input = {
     fontSize: "16px",
     color:"#006",
@@ -15,16 +15,11 @@ class AddForm extends Component {
     color: "#006",
     padding: "1px",
   }
-  messageStyle = {
-    fontSize: "16px",
-    color:"#006",
-    margin: "5px 10px"
-  }
 
   constructor(props) {
     super(props);
     this.state = {
-      message:''
+      number:0
     }
     this.doChange = this.doChange.bind(this);
     this.doAction = this.doAction.bind(this);
@@ -32,30 +27,35 @@ class AddForm extends Component {
 
   doChange(e) {
     this.setState({
-      message: e.target.value
-    });
-  }
-  doAction(e) {
-    e.preventDefault();
-    //ストア
-    let action = addMemo(this.state.message);
-    this.props.dispatch(action);
-    this.setState({
-      message: ''
+      number: e.target.value
     });
   }
 
+  doAction(e) {
+    e.preventDefault();
+    //ストア
+    let action = deleteMemo(this.state.number);
+    this.props.dispatch(action);
+    this.setState({
+      number:0
+    });
+  }
   render() {
-    return (
+    let n = 0;
+    let items = this.props.data.map((value) => (
+      <option key={n} value={n++}>{value.message.subString(0,10)}</option>
+    ));
+    return(
       <div>
-        <p style={this.messageStyle}>{this.props.message}</p>
         <form onSubmit={this.doAction}>
-          <input type="text" size="40" onChange={this.doChange} style={this.input} value={this.state.message} required />
-          <input type="submit" value="add" style={this.btn} />
+          <select onChange={this.doChange} style={this.input}>
+            {items}
+          </select>
+          <input type="submit" style={this.btn} value="del" />
         </form>
       </div>
     )
   }
 }
 
-export default connect ((state) => state)(AddForm);
+export default connect((state) => state)(DelForm);
